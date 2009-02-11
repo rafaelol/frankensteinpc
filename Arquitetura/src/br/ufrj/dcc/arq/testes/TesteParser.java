869baca7bc03256323f476c0c5d311c3c0bb3ad3
diogobor <diogobor@0d417312-c0be-11dd-880d-d7d17d6dbbbe>
2009-02-11 11:28:00 +0000

    Mudança da classe TesteParser do modulo br.gov.ufrj.parse PARA br.ufrj.dcc.arq.testes

---
+++ Arquitetura/src/br/ufrj/dcc/arq/testes/TesteParser.java
@@ -0,0 +1,39 @@
+package br.ufrj.dcc.arq.testes;
+
+import java.util.List;
+
+import br.ufrj.dcc.parse.LerArquivo;
+import br.ufrj.dcc.parse.Parser;
+
+public class TesteParser extends LerArquivo{
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+/*A variavel teste somente esta testando se o LerArquivo esta funcionando.
+ * Ja a variavel parser testa o parser propriamente dito*/		
+		LerArquivo teste = new LerArquivo();
+
+		String[] codigoPrograma = qtdParser.toArray(new String[qtdParser.size()]);
+		
+		Parser parser = new Parser(codigoPrograma, qtdParser.size());
+		
+		/*
+		 * String codigoPrograma[] = new String[6];
+                
+                codigoPrograma[0] = "TESTE: mov <10,R0>";
+                codigoPrograma[1] = "mov <20,(R1)>";
+                codigoPrograma[2] = "add <R0,R1>";
+                codigoPrograma[3] = "neg <R1>";
+                codigoPrograma[4] = "jmp <TESTE>";
+                codigoPrograma[5] = "halt";
+                
+                Parser parser = new Parser(codigoPrograma, 6);
+		 */
+		
+	}
+
+}
