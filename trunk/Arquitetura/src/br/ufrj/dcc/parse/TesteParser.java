533bc9e3f10a8618c411921e4bd86f14e30aeecf
diogobor <diogobor@0d417312-c0be-11dd-880d-d7d17d6dbbbe>
2009-02-11 11:19:10 +0000

    Teste do Parser.

--- trunk/Arquitetura/src/br/ufrj/dcc/parse/TesteParser.java
+++ trunk/Arquitetura/src/br/ufrj/dcc/parse/TesteParser.java
@@ -1,22 +1,35 @@
 package br.ufrj.dcc.parse;
 
-public class TesteParser {
+import java.util.List;
+
+public class TesteParser extends LerArquivo{
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String codigoPrograma[] = new String[6];
+
+/*A variavel teste somente esta testando se o LerArquivo esta funcionando.
+ * Ja a variavel parser testa o parser propriamente dito*/		
+		LerArquivo teste = new LerArquivo();
+
+		String[] codigoPrograma = qtdParser.toArray(new String[qtdParser.size()]);
 		
-		codigoPrograma[0] = "TESTE: mov <10,R0>";
-		codigoPrograma[1] = "mov <20,(R1)>";
-		codigoPrograma[2] = "add <R0,R1>";
-		codigoPrograma[3] = "neg <R1>";
-		codigoPrograma[4] = "jmp <TESTE>";
-		codigoPrograma[5] = "halt";
+		Parser parser = new Parser(codigoPrograma, qtdParser.size());
 		
-		Parser parser = new Parser(codigoPrograma, 6);
+		/*
+		 * String codigoPrograma[] = new String[6];
+                
+                codigoPrograma[0] = "TESTE: mov <10,R0>";
+                codigoPrograma[1] = "mov <20,(R1)>";
+                codigoPrograma[2] = "add <R0,R1>";
+                codigoPrograma[3] = "neg <R1>";
+                codigoPrograma[4] = "jmp <TESTE>";
+                codigoPrograma[5] = "halt";
+                
+                Parser parser = new Parser(codigoPrograma, 6);
+		 */
 		
 	}
 
