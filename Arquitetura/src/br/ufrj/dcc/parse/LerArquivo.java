4348f99ab8943d98d0d281517791ee13cab95ca9
diogobor <diogobor@0d417312-c0be-11dd-880d-d7d17d6dbbbe>
2009-02-11 11:29:46 +0000

    Atualizado os import's...

--- Arquitetura/src/br/ufrj/dcc/parse/LerArquivo.java
+++ Arquitetura/src/br/ufrj/dcc/parse/LerArquivo.java
@@ -1,20 +1,14 @@
 package br.ufrj.dcc.parse;
 
-import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.Iterator;
 import java.util.List;
-import java.util.Map;
-import java.util.SortedMap;
 
 
 public class LerArquivo{
 
-	static ArquivoTexto arqParser;
+	public static ArquivoTexto arqParser;
 
-	static List<String> qtdParser = new ArrayList<String>();
+	public static List<String> qtdParser = new ArrayList<String>();
 
 
 	public LerArquivo(){
