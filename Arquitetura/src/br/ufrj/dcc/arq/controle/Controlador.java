d39352aea4adb834164bec30f7ec15282fd151cc
rafaoliveiralopes <rafaoliveiralopes@0d417312-c0be-11dd-880d-d7d17d6dbbbe>
2009-02-28 17:51:59 +0000

    Código de controlador limpo. Código de Instrucao identado corretamente.

--- Arquitetura/src/br/ufrj/dcc/arq/controle/Controlador.java
+++ Arquitetura/src/br/ufrj/dcc/arq/controle/Controlador.java
@@ -2,7 +2,6 @@ package br.ufrj.dcc.arq.controle;
 
 import javax.swing.JOptionPane;
 
-import br.ufrj.dcc.arq.modelo.Bit;
 import br.ufrj.dcc.arq.modelo.Memoria;
 import br.ufrj.dcc.arq.modelo.Processador;
 import br.ufrj.dcc.arq.modelo.Uc;
@@ -21,8 +20,6 @@ public class Controlador extends Thread {
 	
 	public static Parser parser;
 	
-	private Bit bitMemoria;
-	
 	private int percorreVetorParser;
 	
 	private short mempos;
@@ -32,7 +29,6 @@ public class Controlador extends Thread {
 		
 		proc 		= new Processador();
 		parser 		= new Parser(nomeArq);
-		//bitMemoria 	= new Bit((short)0);
 
 		/*
 		 * Carrega o programa na memoria
@@ -43,77 +39,6 @@ public class Controlador extends Thread {
 		}
 		Comecar.colocarNoListPrograma();
 		Comecar.colocarNaMemoria();
-		// Imprime Posicoes de Memoria
-		/* Comentado. Motivo: Verificado que foi colocado corretamente na memoria. =)
-		System.out.println("\nImpressao da Memoria");
-		for(int i =0; i< parser.getVetorParser().length; i++){
-			System.out.print(proc.memoria.getMemPos(i) + " ");
-		}
-		System.out.println("\nFim da Impressao da Memoria");
-		*/
-		
-		/*
-		proc.registrador.r0 = 1;
-		proc.registrador.r1 = 0;
-		proc.registrador.r2 = 52;
-		proc.registrador.r3 = 53;
-		proc.registrador.r4 = 54;
-		proc.memoria.setMemPos(50, (short)1);
-		proc.memoria.setMemPos(51, (short)1);
-		proc.memoria.setMemPos(52, (short)1);
-		proc.memoria.setMemPos(53, (short)1);
-		proc.memoria.setMemPos(54, (short)1);
-		*/
-		/*
-		System.out.println("POSICAO 50 = " + proc.memoria.getMemPos(50));
-		System.out.println("POSICAO 51 = " + proc.memoria.getMemPos(51));
-		System.out.println("POSICAO 52 = " + proc.memoria.getMemPos(52));
-		System.out.println("POSICAO 53 = " + proc.memoria.getMemPos(53));
-		System.out.println("POSICAO 54 = " + proc.memoria.getMemPos(54));
-		*/		
-		/*
-		 * O executa_programa fica FALSE quando a instrucao eh HALT.
-		 */
-		/*while(proc.executa_programa) {
-			mempos = proc.memoria.getMemPos(proc.registrador.pc);	
-			Comecar.listaMemoria.select(Comecar.retornarIndexListMemoria(String.valueOf(mempos)));
-			proc.uc.decodificaEChama(mempos, executa_por_micro, proc);
-			System.out.println("**********************************");
-			System.out.println("Impressao de Registradores e Flags");
-			System.out.println("**********************************");
-			System.out.println("R0 = " + proc.registrador.r0);
-			System.out.println("R1 = " + proc.registrador.r1);
-			System.out.println("R2 = " + proc.registrador.r2);
-			System.out.println("R3 = " + proc.registrador.r3);
-			System.out.println("R4 = " + proc.registrador.r4);
-			System.out.println("PC = " + proc.registrador.pc);
-			System.out.println("RDADOS = " + proc.registrador.rdados);
-			System.out.println("REND = " + proc.registrador.rend);
-			System.out.println("RI = " + proc.registrador.ri);
-			System.out.println("RX = " + proc.registrador.rx);
-			System.out.println("RY = " + proc.registrador.ry);
-			System.out.println("\n");
-			System.out.println("Flag CARRY    = " + proc.ula.getFlags()[Ula.CARRY]);
-			System.out.println("Flag OVERFLOW = " + proc.ula.getFlags()[Ula.OVERFLOW]);
-			System.out.println("Flag PARIDADE = " + proc.ula.getFlags()[Ula.PARIDADE]);
-			System.out.println("Flag SINAL    = " + proc.ula.getFlags()[Ula.SINAL]);
-			System.out.println("Flag ZERO     = " + proc.ula.getFlags()[Ula.ZERO]);
-			System.out.println("****************");
-			System.out.println("Fim da Impressao");
-			System.out.println("****************");
-			repintaTela(proc);
-			while (executa_por_instrucao) {
-				//espera proximo clique
-			}
-		}*/
-		
-		/*
-		System.out.println("POSICAO 50 = " + proc.memoria.getMemPos(50));
-		System.out.println("POSICAO 51 = " + proc.memoria.getMemPos(51));
-		System.out.println("POSICAO 52 = " + proc.memoria.getMemPos(52));
-		System.out.println("POSICAO 53 = " + proc.memoria.getMemPos(53));
-		System.out.println("POSICAO 54 = " + proc.memoria.getMemPos(54));
-		*/
 	}	
 	
 	public Controlador(String vetProg[], int tamanho) {
@@ -211,11 +136,11 @@ public class Controlador extends Thread {
 					System.out.println("RI = " + proc.registrador.ri);
 					System.out.println("RX = " + proc.registrador.rx);
 					System.out.println("RY = " + proc.registrador.ry);
-					//System.out.println("(R0) = " + proc.memoria.getMemPos(Memoria.ConverteInt(proc.registrador.r0)));
-					//System.out.println("(R1) = " + proc.memoria.getMemPos(Memoria.ConverteInt(proc.registrador.r1)));
-					//System.out.println("(R2) = " + proc.memoria.getMemPos(Memoria.ConverteInt(proc.registrador.r2)));
-					//System.out.println("(R3) = " + proc.memoria.getMemPos(Memoria.ConverteInt(proc.registrador.r3)));
-					//System.out.println("(R4) = " + proc.memoria.getMemPos(Memoria.ConverteInt(proc.registrador.r4)));
+					System.out.println("(R0) = " + proc.memoria.getMemPos(Memoria.ConverteInt(proc.registrador.r0)));
+					System.out.println("(R1) = " + proc.memoria.getMemPos(Memoria.ConverteInt(proc.registrador.r1)));
+					System.out.println("(R2) = " + proc.memoria.getMemPos(Memoria.ConverteInt(proc.registrador.r2)));
+					System.out.println("(R3) = " + proc.memoria.getMemPos(Memoria.ConverteInt(proc.registrador.r3)));
+					System.out.println("(R4) = " + proc.memoria.getMemPos(Memoria.ConverteInt(proc.registrador.r4)));
 					System.out.println("\n");
 					System.out.println("Flag CARRY    = " + proc.ula.getFlags()[Ula.CARRY]);
 					System.out.println("Flag OVERFLOW = " + proc.ula.getFlags()[Ula.OVERFLOW]);
